60cc136d08dbd8c6055853a190c2f6289107888e
Luciano Lupo <lnlupo95@gmail.com>
2022-11-09 16:50:25 -0300

    Correciones Desafio 2

--- Desafio2/main.js
+++ Desafio2/main.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 class Contenedor{
     constructor(archivo){
         this.archivo = archivo;
-        this.arrayObjetos = [];
     }
     async save(obj){
         const arrayObjetos = await this.getAll();
         arrayObjetos.push(obj);
         let indice = arrayObjetos.indexOf(obj);
         if(arrayObjetos.length <= 1){
-            arrayObjetos[indice]['id'] = 1;
+            arrayObjetos[indice]['id']= 1;
         }else{
             arrayObjetos[indice]['id'] = arrayObjetos[indice - 1 ]['id'] + 1;            
         }
@@ -21,39 +20,39 @@ class Contenedor{
             console.log("No se pudo guardar");
         }
     }
-    getById(id){
-        let array = this.getAll();
-        let element = array.find(e => e['id'] == id); 
+    async getById(id){
+        let array = await this.getAll();
+        let element = array.find(e => e.id === id); 
         if(element == undefined){
             element = null;
         }
         return element;
     }
-    getAll(){
+    async getAll(){
         try{
-            const contenido = fs.readFileSync(this.archivo, 'utf-8');
+            const contenido = await fs.promises.readFile(this.archivo, 'utf-8');
             const obj = JSON.parse(contenido);
             return obj;
         }
         catch(err) {
-            return this.arrayObjetos;
+            return [];
         }    
     }
     async deleteById(id){
-        let array = this.getAll();
-        let nuevoArray = array.filter(e => e['id'] != id);
+        let array = await this.getAll();
+        let nuevoArray = array.filter(e => e.id !== id);
         const str = JSON.stringify(nuevoArray, null, 2);
         try{
             await fs.promises.writeFile(this.archivo, str);
         }
         catch(err) {
-            console.log("No se pudo guardar");
+            console.log("No se pudo borrar");
         }
 
     }
     async deleteAll(){
         try{
-            await fs.promises.unlink(this.archivo);
+            await fs.promises.writeFile(this.archivo, []);
         }
         catch{
             console.log("No se pudo eliminar");
@@ -80,21 +79,20 @@ async function ejecutarTodo(){
     await contenedor.save(objeto2);
     await contenedor.save(objeto1);
     console.log("save():\n");
-    console.log(contenedor.getAll());
-    console.log()
+    console.log(await contenedor.getAll())
 
-    console.log("getById(1):\n");
-    console.log(contenedor.getById(1))
+    console.log("getById(1):\n")
+    console.log(await contenedor.getById(1))
 
     await contenedor.deleteById(2);
     console.log()
     console.log("deleteById(2):\n");
-    console.log(contenedor.getAll());
+    console.log(await contenedor.getAll());
 
     await contenedor.deleteAll();
     console.log()
     console.log("deleteAll():\n");
-    console.log(contenedor.getAll());
+    console.log(await contenedor.getAll());
 }
 
 ejecutarTodo();
