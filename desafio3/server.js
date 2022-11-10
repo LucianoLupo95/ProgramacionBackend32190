9b2bfdf7a950a425a8afeb7a92be8a1ce4f86fd3
Luciano Lupo <lnlupo95@gmail.com>
2022-11-09 23:55:58 -0300

    Desafio 3

---
+++ desafio3/server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const fs = require('fs');
+const app = express()
+
+
+
+app.get('/productos', (req, res) => {
+
+    res.send(productos);    
+})
+
+
+const server = app.listen(8080, () =>{
+    console.log('Servidor escuchando en el puerto 8080');
+})
+
+server.on('error', error => console.log('Hubo un error: ' + error))
+
+
+//Contenedor
+class Contenedor{
+    constructor(archivo){
+        this.archivo = archivo;
+    }
+    async save(obj){
+        const arrayObjetos = await this.getAll();
+        arrayObjetos.push(obj);
+        let indice = arrayObjetos.indexOf(obj);
+        if(arrayObjetos.length <= 1){
+            arrayObjetos[indice]['id']= 1;
+        }else{
+            arrayObjetos[indice]['id'] = arrayObjetos[indice - 1 ]['id'] + 1;            
+        }
+        const str = JSON.stringify(arrayObjetos, null, 2);
+        try{
+            await fs.promises.writeFile(this.archivo, str);
+        }
+        catch(err) {
+            console.log("No se pudo guardar");
+        }
+    }
+    async getById(id){
+        let array = await this.getAll();
+        let element = array.find(e => e.id === id); 
+        if(element == undefined){
+            element = null;
+        }
+        return element;
+    }
+    async getAll(){
+        try{
+            const contenido = await fs.promises.readFile(this.archivo, 'utf-8');
+            const obj = JSON.parse(contenido);
+            return obj;
+        }
+        catch(err) {
+            return [];
+        }    
+    }
+    async deleteById(id){
+        let array = await this.getAll();
+        let nuevoArray = array.filter(e => e.id !== id);
+        const str = JSON.stringify(nuevoArray, null, 2);
+        try{
+            await fs.promises.writeFile(this.archivo, str);
+        }
+        catch(err) {
+            console.log("No se pudo borrar");
+        }
+
+    }
+    async deleteAll(){
+        try{
+            await fs.promises.writeFile(this.archivo, []);
+        }
+        catch{
+            console.log("No se pudo eliminar");
+        }
+    }
+};
