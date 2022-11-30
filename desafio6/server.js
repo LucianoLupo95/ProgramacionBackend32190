f4f1513c7498a4e2e7f77b15681d1ca7a753f4a2
Luciano Lupo <lnlupo95@gmail.com>
2022-11-30 10:38:34 -0300

    Desafio 06

--- desafio6/server.js
+++ desafio6/server.js
@@ -1,72 +1,20 @@
 //Modulos y seteos
-const fs = require("fs");
-const express = require("express");
+import express from "express";
+import { Server as HttpServer } from "http";
+import { Server as IOServer } from "socket.io";
+import fs from "fs";
+import Contenedor from "./contenedor.js";
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
-const { Server: HttpServer } = require("http");
-const { Server: IOServer } = require("socket.io");
 const httpServer = HttpServer(app);
 const io = new IOServer(httpServer);
 app.use(express.static("public"));
 
 //Contenedor
-class Contenedor {
-  constructor(archivo) {
-    this.archivo = archivo;
-  }
-  async save(obj) {
-    const arrayObjetos = await this.getAll();
-    arrayObjetos.push(obj);
-    let indice = arrayObjetos.indexOf(obj);
-    if (arrayObjetos.length <= 1) {
-      arrayObjetos[indice]["id"] = 1;
-    } else {
-      arrayObjetos[indice]["id"] = arrayObjetos[indice - 1]["id"] + 1;
-    }
-    const str = JSON.stringify(arrayObjetos, null, 2);
-    try {
-      await fs.promises.writeFile(this.archivo, str);
-    } catch (err) {
-      console.log("No se pudo guardar");
-    }
-  }
-  async getById(id) {
-    let array = await this.getAll();
-    let element = array.find((e) => e.id === id);
-    if (element == undefined) {
-      element = null;
-    }
-    return element;
-  }
-  async getAll() {
-    try {
-      const contenido = await fs.promises.readFile(this.archivo, "utf-8");
-      const obj = JSON.parse(contenido);
-      return obj;
-    } catch (err) {
-      return [];
-    }
-  }
-  async deleteById(id) {
-    let array = await this.getAll();
-    let nuevoArray = array.filter((e) => e.id !== id);
-    const str = JSON.stringify(nuevoArray, null, 2);
-    try {
-      await fs.promises.writeFile(this.archivo, str);
-    } catch (err) {
-      console.log("No se pudo borrar");
-    }
-  }
-  async deleteAll() {
-    try {
-      await fs.promises.writeFile(this.archivo, []);
-    } catch {
-      console.log("No se pudo eliminar");
-    }
-  }
-}
+
 //Productos
 const contenedorProductos = new Contenedor("./productos.txt");
 const contenedorMensajes = new Contenedor("./mensajes.txt");
