02b927e6b6eccf1314e04aff51384894e0eb471d
Luciano Lupo <lnlupo95@gmail.com>
2022-12-07 18:50:59 -0300

    Primer desafio

--- primeraEntrega/server.js
+++ primeraEntrega/server.js
@@ -6,9 +6,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("json spaces", 2);
+app.use(express.static("public"));
 
 app.use("/api/productos", productsRouter);
 app.use("/api/carrito", cartRouter);
+app.get("*", (req, res) => {
+  res.status(404).json({
+    error: -2,
+    description: `Ruta ${req.originalUrl} método GET no definida`,
+  });
+});
+app.post("*", (req, res) => {
+  res.status(404).json({
+    error: -2,
+    description: `Ruta ${req.originalUrl} método POST no definida`,
+  });
+});
+app.delete("*", (req, res) => {
+  res.status(404).json({
+    error: -2,
+    description: `Ruta ${req.originalUrl} método DELETE no definida`,
+  });
+});
+app.put("*", (req, res) => {
+  res.status(404).json({
+    error: -2,
+    description: `Ruta ${req.originalUrl} método PUT no definida`,
+  });
+});
+
 const PORT = process.env.port || 8080;
 const server = app.listen(PORT, () =>
   console.log(`Servidor escuchando en el puerto ${PORT}`)
